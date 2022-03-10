41336a36a72cd7e61e6d3f28d2ce0b9bb6b12d8c
leandroatavares <leandroanjostavares@gmail.com>
2022-03-10 20:29:58 -0300

    cria redirecionamento para pagina de pagamento do stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/react'
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -5,10 +8,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  const { data: session } = useSession();
+
+  async function handleSubscriber() {
+    if(!session) {
+      signIn('github')
+      return;
+    }
+    //criaçao checkout session Stripe
+    try {
+      const response = await api.post('/subscribe/checkout-session')
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+      stripe.redirectToCheckout({sessionId})
+
+    } catch(err) {
+      alert(err.message) // TODO -> incluir toast
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscriber}
     >
       Subscribe now
     </button>
