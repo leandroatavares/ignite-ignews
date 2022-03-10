b9ac7ecea1beca18344dbe6f0d3f2750160f2999
leandro.tavares <leandro.tavares@original.com.br>
2022-03-10 19:16:48 -0300

    realiza criaçao e update de usuario no BD(faunadb)

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from "next-auth/providers/github"
+import { query as q, Update } from 'faunadb';
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,6 +16,53 @@ export default NextAuth({
       }
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {
+                data: {
+                  email: user.email,
+                  lastLogin: Date.now()
+                }
+              }
+            ),
+            q.Update(
+              q.Select("ref", q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )),
+              {
+                data: {
+                  lastLogin: Date.now()
+                }
+              }
+            )
+          )
+        )
+
+        return true;
+      } catch {
+        return false;
+      }
+
+     
+    },
+  }
 
   // A database is optional, but required to persist accounts in a database
   // database: process.env.DATABASE_URL, //old but possible
